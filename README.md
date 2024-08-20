bbeacfceb924ca1750220f70b4531e7c2e4b4e23
retaj <retajharbi319@gmail.com>
2024-08-20 09:08:20 +0300

    three D

--- README.md
+++ README.md
@@ -7,5 +7,15 @@ Currently, two official plugins are available:
 - [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
 - [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
 ### `npm create vite@latest my-project -- --template react`
+
 ### `npm install -D tailwindcss postcss autoprefixer`
+
 resource (https://tailwindcss.com/docs/guides/vite)
+### `npm install gsap`
+resource (https://gsap.com/docs/v3/Installation/?tab=npm&module=esm&method=private+registry&tier=free&club=false&require=false&trial=true)
+
+### `npm i split-type`
+
+
+### `clip-path`
+resource (https://www.cssportal.com/css-clip-path-generator/)
--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,80 +1,51 @@
 import gsap from "gsap";
-import { useEffect, useRef } from "react";
 import { ScrollTrigger } from "gsap/all";
+import { useGSAP } from "@gsap/react";
+import { useEffect } from "react";
+import SplitType from "split-type";
 
 gsap.registerPlugin(ScrollTrigger);
 
 const Intro = () => {
-  const introRef = useRef(null);
-
+  const mytext1 = new SplitType("#intro1");
+  gsap.to(mytext1.chars, {
+    y: 0,
+    stagger: 0.1,
+    delay: 0.2,
+    duration: 0.1,
+  });
   useEffect(() => {
-
-    gsap.to("#intro", {
-      opacity: 0,
-      y: 50,
-    });
-
-    gsap.to("#intro2", {
-      opacity: 0,
-      y: 50,
-    });
-
-    ScrollTrigger.create({
-      trigger: "#intro",
-      start: "top top",
-      end: "bottom bottom",      
-      scrub: true,
-      onEnter: () => {
-        gsap.to("#intro", {
-          opacity: 1,
-          delay: 1.5,
-          color: "white",
-          y: 0,
-        });
-      },
-      onLeaveBack: () => {
-        gsap.to("#intro", {
-          opacity: 0,
-          y: 50,
-        });
+    const mytext2 = new SplitType("#intro2");
+    gsap.to(mytext2.chars, {
+      y: 0,
+      stagger: 0.1,
+      delay: 0.2,
+      duration: 0.1,
+      scrollTrigger: {
+        trigger: "#intro2",
+        start: "top center",
+        end: "bottom 20%",
       },
     });
-
-    ScrollTrigger.create({
-      trigger: "#intro2",
-      start: "top 400px",
-      scrub: true,
-      onEnter: () => {
-        gsap.to("#intro2", {
-          opacity: 1,
-          delay: 1.5,
-          color: "white",
-          y: 0,
-        });
-      },
-      onLeaveBack: () => {
-        gsap.to("#intro2", {
-          opacity: 0,
-          y: 50,
-        });
-      },
-    });
-
   }, []);
 
   return (
-    <section ref={introRef} className="w-full text-center nav-height bg-black relative">
-      <div className="h-5/6 w-full flex-center flex-col">
-        <h1 id="intro" className="intro-title">
-          CREATIVE MIND
-        </h1>
-      </div>
-      <div className="h-5/6 w-full flex-center flex-col">
-        <h1 id="intro2" className="intro-title">
-          Developer and Designer
-        </h1>
-      </div>
-    </section>
+    <div className="mt-20 w-full h-screen">
+      <section className="w-full text-center nav-height bg-black relative ">
+        <div className="h-1/6 w-full flex-center flex-col">
+          <h1 id="intro1" className="intro-title">
+            CREATIVE MIND
+          </h1>
+        </div>
+      </section>
+      <section className="w-full text-center nav-height bg-black relative">
+        <div className="h-5/6 w-full flex-center flex-col">
+          <h1 id="intro2" className="intro-title">
+            Developer and Designer
+          </h1>
+        </div>
+      </section>
+    </div>
   );
 };
 
